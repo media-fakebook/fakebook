b06789b78253b132685688c47cbcf1ee06eacc12
gch01410 <gch01410@naver.com>
2020-08-02 22:18:15 +0900

    init project

---
+++ src/main/java/com/fakebook/fakebook/FakeBookApplication.java
@@ -0,0 +1,13 @@
+package com.fakebook.fakebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fakebook/fakebook/FakeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakebook.fakebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
